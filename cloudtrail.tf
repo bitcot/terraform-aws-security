00387686024de3ac07d17f6f088ef162343a26f6
BitCot <raj@bitcot.com>
2021-11-25 10:31:56 +0530

    CloudTrail log file validation is enabled

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -4,6 +4,7 @@ resource "aws_cloudtrail" "cloudtrail" {
     s3_key_prefix = "prefix"
     include_global_service_events = true
     is_multi_region_trail =  true
+    enable_log_file_validation = true
 }
 
 resource "aws_s3_bucket" "cloudfrontbucket" {
